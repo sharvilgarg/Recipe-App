fe20dc9fc384bf00d9cd949694dfe36a9e774f2c
sharvilgarg <sharvil.garg@gmail.com>
2019-05-30 10:32:22 +0530

    Recipe Start

---
+++ src/main/java/com/project/recipeapp/RecipeAppApplication.java
@@ -0,0 +1,13 @@
+package com.project.recipeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipeAppApplication.class, args);
+    }
+
+}
